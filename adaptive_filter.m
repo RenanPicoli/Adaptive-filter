f6393450067b6715f9d451bbbe96cffabcd46b9c
RenanPicoli <renanps123@gmail.com>
2020-10-10 19:40:16 -0300

    migrated algorithm to function adaptive_filter(); also added useful handlers

--- adaptive_filter.m
+++ adaptive_filter.m
@@ -1,40 +1,40 @@
 %Objetivo: fazer um filtro cuja saída y para a excitação x seja igual a resposta desejada d (ou seja, descobrir o filtro u usado)
 %x: input: input signal
-%u: input: Entre com os coeficientes do filtro desconhecido
-%N: input: Entre com o número de coeficientes que deseja usar
-%tol: input: Entre com o valor da tolerância no erro entre y e d (norma euclidiana)
+%d: input: Resposta do filtro desconhecido à entrada x
+%N: input: Nnúmero máximo de coeficientes do filtro
+%tol: input: Tolerância no erro entre y e d (norma euclidiana)
 %y: output: saída do FA para entrada x
 %w: output: filtro obtido
+%err: output: erros em cada iteração (y-d)
+%filter_mat: output: filtros intermediários obtidos
 %n: output: número de iteracoes usadas
 
-function [y,w,n] = adaptive_filter(x,d,N=6,tol=1e-5)
+function [y,w,err,filter_mat,n] = adaptive_filter(x,d,N=6,tol=1e-5)
 L=length(x);
 err=zeros(1,L);
-xN=zeros(1,N);% vector with last N samples
-filter=zeros(1,N,L);
-step_grad=1;
+xN=zeros(1,N);% vector with last N inputs
+filter_mat=zeros(1,N,L);
+% para o método do gradiente descendente
+step_grad = 1;
 
 % itera sobre as amostras
 for n=1:L % cálculo de filtro em n+1 usando filtro em n
+  
+    %xN contém as últimas N entradas
     xN = [x(n) xN(1:N-1)];
-	err(n)=d(n) - filter(:,:,n)*xN.';% aproximação do erro: xN é aproximação do sinal completo
+	  err(n)=d(n) - filter_mat(:,:,n)*xN.';% aproximação do erro: xN é aproximação do sinal completo
     
     if (xN*xN.' ~= 0)
         delta_filter = step_grad*err(n)*xN/(xN*xN.');
-        filter(:,:,n+1) = filter(:,:,n) + delta_filter;
+        filter_mat(:,:,n+1) = filter_mat(:,:,n) + delta_filter;
         % testa se já convergiu
-        if(norm(delta_filter)/norm(filter(:,:,n)) < tol)
+        if(norm(delta_filter)/norm(filter_mat(:,:,n)) < tol)
             break;
         end
     end
-    n=n+1;	
+    n=n+1;
 end
-
 n=n-1;
-w=filter(:,:,n);
+w=filter_mat(:,:,n);
 y=conv(w,x);
-disp('Filtro calculado');
-disp(w)
-disp('Número de iterações usadas:')
-disp(n)
 end
--- applied_in_music.m
+++ applied_in_music.m
@@ -13,9 +13,6 @@ close all
 % y(n) = b0x(n)+..+bPx(n-P)
 u=[0.5 sqrt(2)/2 0.5]%input("Entre com os coeficientes do filtro desconhecido:")
 
-% para o método do gradiente descendente
-step_grad = 1;
-
 [original,fs] = audioread('Rise From The Ashes.ogg');
 
 x = original(:,1);% x foi gravada com 2 canais, vamos pegar apenas o primeiro
@@ -38,42 +35,24 @@ d=filter(u,[1],x);% d of desired response, same length as x. Could be conv(u,x)
 n=1; % index of iteration being performed/ sample being taken into account
 
 N=6;%input("Entre com o número de coeficientes que deseja usar:")
-L=length(x);
-err=zeros(1,L);
-xN=zeros(1,N);% vector with last N inputs
-filter_mat=zeros(1,N,L);
 
 % para convergir: erro percentual entre dois filtros consecutivos 
 tol = 1e-6;% |h(n)-h(n-1)| / |h(n)|
 
-% itera sobre as amostras
-for n=1:L % cálculo de filtro em n+1 usando filtro em n
-  
-    %xN contém as últimas N entradas
-    xN = [x(n) xN(1:N-1)];
-	  err(n)=d(n) - filter_mat(:,:,n)*xN.';% aproximação do erro: xN é aproximação do sinal completo
-    
-    if (xN*xN.' ~= 0)
-        delta_filter = step_grad*err(n)*xN/(xN*xN.');
-        filter_mat(:,:,n+1) = filter_mat(:,:,n) + delta_filter;
-        % testa se já convergiu
-        if(norm(delta_filter)/norm(filter_mat(:,:,n)) < tol)
-            break;
-        end
-    end
-    n=n+1;
-end
-n=n-1;
+L=length(x);
+
+[y,w,err,filter_mat,n] = adaptive_filter(x,d,N,tol);
+
 disp('Número de iterações usadas:')
 disp(n)
 
-w=filter_mat(:,:,n);
 disp('Filtro calculado');
 disp(w)
 
+hcoeff=zeros(1,N);
 for i=1:N
   figure
-  plot(filter_mat(:,i,1:n))
+  hcoeff(i)=plot(filter_mat(:,i,1:n));
   hold on
   if (i <= length(u))
     plot(u(i)*ones(1,n))
@@ -86,9 +65,9 @@ for i=1:N
 end
 
 figure
-plot(d,'-b')
+hd=plot(d,'-b');
 hold on
-plot(conv(w,x),'-r')
+hy=plot(y,'-r')
 title('Respostas')
 ##xt = min_x*downsample_factor/fs:max_x*downsample_factor/fs;
 ##% altera a unidade do eixo x de amostras para segundos
@@ -98,13 +77,13 @@ grid on
 legend('filtro desconhecido','filtro adaptativo')
 
 figure
-stem(err(1:n))
+herr=stem(err(1:n));
 title('Erro')
 grid on
 
 % algumas vezes pode ser mais fácil observar em escala logaritmica
 figure
-plot(20*log10(abs(err(1:n))))
+herrdB=plot(20*log10(abs(err(1:n))));
 title('Erro em dB')
 grid on
 
@@ -120,14 +99,14 @@ norma_erro_filtro=zeros(1,n);
 for i=1:n
     norma_erro_filtro(i)=20*log10(norm(filter_mat(:,:,i) - [u zeros(1,N-length(u))]));% erro em dB
 end
-plot(norma_erro_filtro);
+hnormaerr=plot(norma_erro_filtro);
 grid on
 title('2-norma do erro entre os filtros em dB')
 
 % checking these plots, we see if our filter rapidly converges to the
 % unknown filter
 
-figure
+hfiltros=figure;
 hold on
 for i=1:10:n
     plot(filter_mat(:,:,i));
